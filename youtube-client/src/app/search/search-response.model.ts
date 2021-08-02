ebb9a4d75b310b181b95b2f8dca8284a9ade81ae
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2021-08-02 21:30:58 +0300

    feat: interfaces search

--- youtube-client/src/app/search/search-response.model.ts
+++ youtube-client/src/app/search/search-response.model.ts
@@ -1,1 +1,13 @@
-export interface SearchResponse {}
+import { Item } from './search-item.model';
+
+export interface Response {
+  kind: string;
+  etag: string;
+  pageInfo: PageInfo;
+  items: Item[];
+}
+
+interface PageInfo {
+  totalResults: number;
+  resultsPerPage: number;
+}
