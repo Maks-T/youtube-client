3461a73b53d7b637b598998e0599420bad37a304
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2021-09-01 22:03:50 +0300

    feat: add validators admin form

--- youtube-client/src/app/youtube/components/admin/admin.component.ts
+++ youtube-client/src/app/youtube/components/admin/admin.component.ts
@@ -18,15 +18,20 @@ export class AdminComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    public store: Store<IAppState>,
+    public store: Store<IAppState>
   ) {}
 
   ngOnInit(): void {
+    const regUrlImage =
+      '^https?://(?:[a-z0-9-]+.)+[a-z]{2,6}(?:/[^/#?]+)+.(?:jpg|gif|png)$';
+    const regUrlVideo =
+      '^https?://(?:[a-z0-9-]+.)+[a-z]{2,6}(?:/[^/#?]+)+.(?:mov|avi|wmv|flv|3gp|mp4|mpg)$';
+
     this.cardForm = this.fb.group({
-      title: ['', [Validators.required]],
-      description: ['', [Validators.required]],
-      imageLink: ['', [Validators.required]],
-      videoLink: ['', [Validators.required]],
+      title: ['', [Validators.required, Validators.minLength(2)]],
+      description: ['', [Validators.required, Validators.minLength(10)]],
+      imageLink: ['', [Validators.required, Validators.pattern(regUrlImage)]],
+      videoLink: ['', [Validators.required, Validators.pattern(regUrlVideo)]],
     });
   }
 
@@ -62,5 +67,45 @@ export class AdminComponent implements OnInit {
     this.store.dispatch(new CreateCustomItem(payload));
 
     this.router.navigate(['search']);
+  }
+
+  getTitleErrorMessage(): string {
+    const title = this.cardForm.controls.title;
+    if (title.hasError('required')) {
+      return 'You must enter a Title';
+    }
+    return title.hasError('minlength')
+      ? 'Title must contain more than 2 characters'
+      : '';
+  }
+
+  getDescriptionErrorMessage(): string {
+    const description = this.cardForm.controls.description;
+    if (description.hasError('required')) {
+      return 'You must enter a Description';
+    }
+    return description.hasError('minlength')
+      ? 'Description must contain more than 10 characters'
+      : '';
+  }
+
+  getImageLinkErrorMessage(): string {
+    const url = this.cardForm.controls.imageLink;
+    if (url.hasError('required')) {
+      return 'You must enter a URL';
+    }
+    return url.hasError('pattern')
+      ? 'Enter the correct URL Image jpg|gif|png'
+      : '';
+  }
+
+  getVideoLinkErrorMessage(): string {
+    const url = this.cardForm.controls.videoLink;
+    if (url.hasError('required')) {
+      return 'You must enter a URL';
+    }
+    return url.hasError('pattern')
+      ? 'Enter the correct URL Video mov|avi|wmv|flv|3gp|mp4|mpg'
+      : '';
   }
 }
